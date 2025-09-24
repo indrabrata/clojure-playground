4b88f5971c7104342d7d543865caa413e0ad24ad
Gede Indra Adi Brata <indrabrata599@gmail.com>
2025-09-24 12:39:41 +0700

    add declaration and core function

--- src/clojure_playground/core.clj
+++ src/clojure_playground/core.clj
@@ -12,4 +12,76 @@
   (- (+ 7 5)  (* 3 8 5))
   ) 
 
-(def var-a 1)
+(def my-var 1)
+(println my-var)
+
+;; There is no return in clojure, it alwas the latest argument will be return
+(def my-fn ( fn []
+  (println (+ 1 2))
+  "calculated")
+  )
+(println my-fn)
+
+(def add (fn [x y] (+ x y)))
+(println (add 1 2))
+
+;; Function scope
+(let [a 1]
+  (println a)
+  (let [a 10]   (println a))
+  )
+
+(let [x 1 y 2 result (add x y)]
+  (println result)
+  )
+
+;; Vector
+(def my-vector [1, 2, 3, 4, 5])
+(get my-vector 0)
+
+;; Map
+;; Dynamic key
+{"a" 1, "b" 2}
+{"a" [1, 2], 1 2, { 1 2} 5}
+
+;; Set
+#{1, 2, 3 ,4 ,5}
+(set [1 2 3 4 5 5])
+
+(def v [1 2 3 4 5 6 7])
+
+;; map function
+;; inc in increment
+(map (fn [e1 e2] 
+       (+ e1 e2)
+       ) v v)
+
+
+;; Filter
+(filter (fn [element] 
+          (even? element)) v )
+
+;; Remove
+(remove (fn[e]
+          (even? e)) v)
+
+;; Reduce
+;; The 0 is start of the accumulator
+;; Have control of the reduce logic
+(reduce (fn [acc e] (+ e acc)) 0 v)
+(reduce (fn [acc e] (conj acc (+ e e)))[] v)
+(reduce (fn [acc e] (let [result (conj acc (+ e e))] (println result) result)) [] v)
+;; Implement some kind of logic to all vector
+(apply + v)
+
+;; Group By
+(group-by (fn [e]
+            (
+             even? e
+            )) v)
+
+;; Count
+(count v)
+
+;; Frequency
+(frequencies v)
